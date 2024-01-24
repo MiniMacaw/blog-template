e52fdc436c19f38aae066c09553e7f68a3372a0a
mhpsy <object@tanyu.mobi>
2024-01-25 00:17:52 +0800

    add: add dark mode and todo.md

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# minimacaw/blog todolist
+
+- [ ] confirm base color, font, and layout .....
+- [ ] dark mode
+- [ ] markdown to json
+- [ ] render markdown
+- [ ] publish
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +9,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        p: '#3490dc', // primary
+        s: '#ffed4a', // secondary
+        a: '#f9acaa', // accent
+        white: '#ffffff', // white
+        black: '#000000', // black
+        dark: {
+          p: '#1d4ed8', // primary
+          s: '#fbbf24', // secondary
+          a: '#f87171', // accent
+          white: '#ffffff', // white
+          black: '#000000', // black
+        }
       },
     },
   },
