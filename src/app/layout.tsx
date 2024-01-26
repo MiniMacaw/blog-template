bd1ef0acfba828ce0fdfa7aed5ad603b6aad375e
mhpsy <object@tanyu.mobi>
2024-01-26 23:53:22 +0800

    update:refactor file strucur and use app route and theming use root-theme key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import "../styles/globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {Providers} from "./providers"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
