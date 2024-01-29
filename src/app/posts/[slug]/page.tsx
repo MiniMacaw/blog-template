8ea381b9eb234f550e6ecbaef1b5d1bba02fb297
Yao Wang <dell9459@gmail.com>
2024-01-29 10:52:31 -0500

    adding pages for posts to the repo

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function Post() {
+    return (
+        <div className="h-[200vh]">
+            <div className=" sticky top-100">
+                test go top the top
+            </div>
+        </div>
+    )
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+export default function Posts() {
+    return (
+        <div className="h-[200vh]">
+            <div className=" sticky top-100">
+                test go top the top
+            </div>
+        </div>
+    )
+}
