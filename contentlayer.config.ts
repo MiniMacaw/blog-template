184169fee3dcfeaffda391dca34270bdbfde1809
Yao Wang <dell9459@gmail.com>
2024-02-12 18:20:05 -0500

    update contentlayer config file, page.tsx on /blog

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -7,6 +7,7 @@ export const Post = defineDocumentType(() => ({
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
+    duration: {type: 'string', required: true},
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/posts/${post._raw.flattenedPath}` },
