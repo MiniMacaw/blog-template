f417bd516c73dcc025957ace0899b833e1066e1f
Yao Wang <dell9459@gmail.com>
2024-02-12 13:10:17 -0500

    update the blog title

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
   return (
     <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
+      <h1 className="mb-8 text-center text-2xl font-black">My Blogs</h1>
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
